1486ce8037a038866d264f34c56fd8d6326f9412
Michael Medina <michael.cunha97@hotmail.com>
2022-02-12 20:41:25 -0300

    Adicionando Calculadora feita usando o framework Vue.js

---
+++ src/main/Calculator.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="calculator">
+      <Display :value="displayValue"/>
+      <Button label="AC" triple @click="clearMemory"/>
+      <Button label="/" operation @click="setOperation" />
+      <Button label="7" @click="addDigit"/>
+      <Button label="8" @click="addDigit"/>
+      <Button label="9" @click="addDigit"/>
+      <Button label="*" operation @click="setOperation" />
+      <Button label="4" @click="addDigit"/>
+      <Button label="5" @click="addDigit"/>
+      <Button label="6" @click="addDigit"/>
+      <Button label="-" operation @click="setOperation"/>
+      <Button label="1" @click="addDigit"/>
+      <Button label="2" @click="addDigit"/>
+      <Button label="3" @click="addDigit"/>
+      <Button label="+" operation @click="setOperation" />
+      <Button label="0" @click="addDigit" double />
+      <Button label="." @click="addDigit"/>
+      <Button label="=" operation @click="setOperation"/>
+  </div>
+</template>
+
+<script>
+
+import Display from '../components/Display.vue'
+import Button from '../components/Button.vue' 
+
+export default {
+    components: {Display, Button},
+    data(){
+        return{
+            displayValue:"0",
+            clearDisplay: false,
+            operation:null,
+            values:[0, 0],
+            current: 0
+
+        }
+    },
+    methods:{
+        clearMemory(){
+            Object.assign(this.$data, this.$options.data())
+        },
+
+        setOperation(operation){
+            if(this.current === 0){
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else{
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch (error) {
+                    this.$emit('onError', error)
+                }
+                this.values[1] = 0
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals 
+            }
+        },
+
+        addDigit(n){
+            if(n === '.' && this.displayValue.includes('.')){
+                return 
+            }
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? " " : this.displayValue
+            const displayValue = currentValue + n 
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            if(n !== "."){
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue 
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.calculator{
+    height: 320px;
+    width: 235px;
+    border-radius:5px;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+
+}
+</style>
